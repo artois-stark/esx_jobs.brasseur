bec4f9724d1918271c4397e3c1105e32f3223664
artois-stark <33905232+artois-stark@users.noreply.github.com>
2018-01-03 21:01:24 +0100

    Create esx_brasseur.sql

---
+++ esx_brasseur.sql
@@ -0,0 +1,11 @@
+INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+('brasseur', 0, 'employee', 'employer', 50, '{}', '{}'),
+
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+('brasseur', 'Brasseur', 0);
+
+INSERT INTO `items` (`name`, `label`) VALUES
+('orge', 'orge germé'),
+('fut_biere', 'Fut de Bière'),
+('bout_biere', 'Bouteille de Bière')
+;
